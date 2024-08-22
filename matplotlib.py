8ff2618109acf02e95608e4e1f5ce57f67b55fd7
Danial Ghofrani <danialghofrani2004@gmail.com>
2024-08-22 12:44:43 +0330

    S1 AND S2

--- matplotlib.py
+++
@@ -1,5 +0,0 @@
-import matplotlib
-matplotlib.use('Agg')  # Use a backend that doesn't require a display server
-from matplotlib import pyplot as plt
-plt.plot(2,3,4)
-plt.show
--- s_1.py
+++ s_1.py
@@ -1,5 +1,4 @@
 # مراحل تحلیل داده
-
 # 1.Data extraction  2.Data cleaning 3.Data normalization(scaling) 4.analysis 5.feature extraction 6.process with AI model  & visualization in all stages
 
 # students = [
@@ -12,23 +11,37 @@
 
 
 import pandas as pd
-import matplotlib
 # pd.read_csv()
 # pd.read_excel()
 # pd.read_json()
+#pd.read_xml()
 # pd.read_clipboard()
 # pd.read_html()
-
-data = pd.read_csv("pokemon_data.csv")
-
-# import yfinance as yf
+# data = pd.read_csv("pokemon_data.csv")
 #
-# BTC = yf.download(tickers:"BTC-USD", start="2024-07-1", interval="30m")
+# data.to_excel()
+# data.to_csv()
 
-print(data.head())
-print(data["Type 1"])
-data_file= data["Speed"]
-plt.plot(data_file)
-
-# plt.legand()
+#
+# import yfinance as yf
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
+# BTC = yf.download("BTC-USD", start="2019-07-12")
+# print(BTC)
+# open_price =BTC["Open"]
+# print(BTC[["Open", "Close"]])
+#
+#
+# plt.plot(open_price)
+#
+# # plt.legand()
 # plt.show()
+
+import streamlit as st
+
+st.title("Data Analysis 01")
+
+if st.button("View BTC"):
+
+    btc = pd.read_csv("D:\programming\data_analysis_mesbah\Mesbah_Data_analysis/BTC-USD.csv")
+    st.write(btc)

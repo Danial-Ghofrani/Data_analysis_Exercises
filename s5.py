584d35ddbbb146f1497613a89608353f771e129d
Danial Ghofrani <danialghofrani2004@gmail.com>
2024-09-03 23:45:53 +0330

    new updates

--- s5.py
+++ s5.py
@@ -55,4 +55,99 @@ import seaborn as sns
 
 
 from sklearn.preprocessing import StandardScaler,MinMaxScaler,LabelEncoder
+from matplotlib import pyplot as plt
+import yfinance as yf
+
+# btc = yf.download('BTC-USD', start='2021-01-01')
+# eth = yf.download('ETH-USD', start='2021-01-01')
+#
+# btc_close = btc["Close"].values.reshape(-1, 1)
+# eth_close = eth["Close"].values.reshape(-1, 1)
+#
+# #(x- mean ) / std   mean = 0 , std = 1
+#
+# # scaler = StandardScaler()
+# scaler = MinMaxScaler(feature_range=(0,50))
+#
+# btc_close = scaler.fit_transform(btc_close)
+# eth_close = scaler.fit_transform(eth_close)
+#
+#
+# plt.plot(btc_close)
+# plt.plot(eth_close)
+# plt.show()
+
+# result = ["bimar", "salem", "mashkook", "salem", "salem", "bimar", "bimar", "bimar", "mashkook"]
+# encoder = LabelEncoder()
+# encoder.fit(result)
+#
+# t_result = encoder.transform(result)
+#
+# print(t_result)
+# encoder.inverse_transform([2])
+
+import pandas as pd
+
+# X1 = [{"id":1, "name":"ali", "age":20}, {"id":2, "name":"reza", 'age':17},{"id":3, "name":'mohsen', "age":22}]
+# X2 = [{"id":1, "name":'ali', "age":20}, {"id":10, "name":"ahmad", "age":25}]
+#
+# df1 = pd.DataFrame(X1)
+# df2 = pd.DataFrame(X2)
+#
+# new_df = pd.concat([df1, df2], axis = 1)
+# new_df.columns = list("abcdef")
+#
+# new_df.reset_index(inplace = True)
+# print(new_df.drop(columns=['index']))
+
+
+
+# X1 = [{"id":1, "name":"ali", "age":20}, {"id":2, "name":"reza", 'age':17},{"id":3, "name":'mohsen', "age":22}]
+# X2 = [{"id":1, "math": 18, "art": 18}, {"id": 3, "math": 18, "art": 18}, {"id": 4, "math": 18, "art": 18}]
+#
+# df1 = pd.DataFrame(X1)
+# df2 = pd.DataFrame(X2)
+#
+# # left brings all of the data in the first data, and right does the same for the second one
+# new_df = pd.merge(df1, df2, on = "id", how = "inner")
+# # new_df = pd.merge(df1, df2, on = "id", how = "outer")
+# # new_df = pd.merge(df1, df2, on = "id", how = "left")
+# # new_df = pd.merge(df1, df2, on = "id", how = "right")
+# # new_df = pd.merge(df1, df2, on = "id", how = "cross")
+#
+# new_df.reset_index(inplace=True)
+# print(new_df.drop(columns=['index']))
+
+# X = [{"id":1, "name":"ali", "age":20}, {"id":2, "name":"reza", 'age':17},{"id":3, "name":'mohsen', "age":22}]
+# df = pd.DataFrame(X)
+#
+# df["name"].str.replace(",","")
+# df["Data"].dt.
+
+# btc = yf.download("BTC-USD", start = "2021-01-01")
+# btc["Date"] = btc.index
+#
+# print(btc["Date"].dt.weekday)
+
+# y = np.array([1,2,3,4,5,4,3,2,1,2,3,4,5,4,3,2,1])
+y = np.random.randint(1, 10, 1000)
+
+X = np.arange(len(y))
+# kernel = [4, 5, 4]
+kernel = np.random.randint(1,10,10)
+
+kernel_size = len(kernel)
+rate = 0.7
+
+plt.plot(X, y)
+
+for i in range(0, len(y) - kernel_size):
+    print(y[i:i + kernel_size] - kernel)
+    sa = np.sum(np.abs(y[i:i + kernel_size] - kernel))
+    ss = np.sum(np.power(y[i:i + kernel_size] - kernel, 2))
+    similarity = np.sum(kernel) - sa
+    if similarity > np.sum(kernel) * rate:
+        plt.plot(X[i:i+kernel_size], kernel, "r--")
+
+plt.show()
 

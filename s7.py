02bde327f0fb386304a98a6ba5646c9c5e6cc852
Danial Ghofrani <danialghofrani2004@gmail.com>
2024-09-06 23:49:32 +0330

    more image processing!

--- s7.py
+++ s7.py
@@ -13,41 +13,148 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-all_news_dict = []
+# all_news_dict = []
+#
+# url = "https://akharinkhabar.ir/"
+#
+# for group in ["sport", "cinema", "money"]:
+#
+#     driver = webdriver.Chrome()
+#     driver.get(f'{url}/{group}')
+#
+#     # print(driver.find_element(By.TAG_NAME('article')))
+#     # print(driver.find_elements(By.CLASS_NAME("rectangle_container__rBE5L")))
+#
+#
+#
+#     # #id
+#     # .class
+#     # tag
+#
+#
+#     news_list = driver.find_elements(By.CSS_SELECTOR,".rectangle_container__rBE5L")
+#     for news in news_list:
+#         my_list = news.text.split("\n")
+#         news_dict = {
+#             "group": my_list[0],
+#             "time": my_list[2],
+#             "time": my_list[3],
+#             "like": my_list[4],
+#             "comments": my_list[5],
+#             "view": my_list[6],
+#         }
+#         all_news_dict.append(news_dict)
+#
+#     driver.close()
+#
+# import pandas as pd
+#
+# df = pd.DataFrame(all_news_dict)
+# df.to_excel("news.xlsx", index=False)
 
-url = "https://akharinkhabar.ir/"
 
-for group in ["sport", "cinema", "money"]:
-
-    driver = webdriver.Chrome()
-    driver.get(f'{url}/{group}')
-
-    # print(driver.find_element(By.TAG_NAME('article')))
-    # print(driver.find_elements(By.CLASS_NAME("rectangle_container__rBE5L")))
-
-
-
-    # #id
-    # .class
-    # tag
-
-
-    news_list = driver.find_elements(By.CSS_SELECTOR,".rectangle_container__rBE5L")
-    for news in news_list:
-        my_list = news.text.split("\n")
-        news_dict = {
-            "group": my_list[0],
-            "time": my_list[2],
-            "time": my_list[3],
-            "like": my_list[4],
-            "comments": my_list[5],
-            "view": my_list[6],
-        }
-        all_news_dict.append(news_dict)
-
-    driver.close()
-
+from datetime import datetime,timedelta
 import pandas as pd
+import numpy as np
 
-df = pd.DataFrame(all_news_dict)
-df.to_excel("news.xlsx", index=False)
+# df = pd.read_excel('news.xlsx')
+
+
+
+# df["time"] = df["time"].str.replace("دقیقه پیش", "")
+# df["time"] = df['time'].str.replace("-", "")
+# df["time"] = df["time"].str.strip()
+#
+# df["time"] = pd.to_numeric(df["time"])
+
+
+# df["like_count"] = df['like'].apply(lambda lk: float(lk.replace('K', "")) * 1000 if "K" in lk else float(lk))
+
+
+# print(df)
+# print(df.info)
+
+# from hazm import *
+#
+# # text = "در دیدارهای دیگر امروز بحرین میزبانش، استرالیا، را یک بر صفر شکست داد تا خالق اولین شگفتی باشد. کره‌جنوبی در خانه با فلسطین مساوی کرد و داد هوادارانش را درآورد. ژاپن، اما، در نخستین دیدارش توانست با اقتدار کامل ۷ بر صفر چین را با سرمربی‌گری برانکو ایوانکوویچ شکست دهد؛ نتیجه‌ای که رسانه‌های چین را به‌سرعت‌ برآشفت."
+#
+# normalizer = Normalizer()
+
+
+# norm_text = normalizer.normalize(text)
+# print(norm_text)
+#
+# sentences = sent_tokenize(norm_text)
+# words = word_tokenize(norm_text)
+#
+# cleaned_word = [word for word in words if word not in stopwords_list()]
+# print(words)
+# print(cleaned_word)
+#
+# stemmer = Stemmer()
+#
+# for word in cleaned_word:
+#     print(word, stemmer.stem(word))
+
+# lemmatizer = Lemmatizer()
+# for word in cleaned_word:
+#     print(word, lemmatizer.lemmatize(word))
+
+# tagger = POSTagger(model="pos_tagger.model")
+# for sentence in sentences:
+#     print(tagger.tag(word_tokenize(sentence)))
+
+
+# from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+#
+# count_vectorizer = CountVectorizer()
+# count_vector = count_vectorizer.fit_transform(cleaned_word)
+# print(count_vector)
+#
+#
+# tfidf_vectorizer = TfidfVectorizer()
+# tfidf_vector = tfidf_vectorizer.fit_transform(cleaned_word)
+# print(tfidf_vector)
+#
+#
+# from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+# text = "man ali man reza shoma"
+# words = np.array(text.split("")).reshape(-1, 1)
+# encoder = LabelEncoder()
+# print(encoder.fit_transform(words))
+#
+# one_hot_encoder = OneHotEncoder()
+# print(one_hot_encoder.fit_transform(words))
+
+
+# import pandas as pd
+#
+# X = np.arange(1, 100).reshape(-1,1)
+# weights = np.array([50,1,2,3,4,5]).reshape(-1, 1)
+#
+#
+# df = pd.DataFrame(X)
+#
+# my_list = list(df.rolling(5).mean())
+#
+# for item in my_list:
+#     if len(item) == 5:
+#         print(item.values * weights)
+
+
+
+# import pandas as pd
+#
+# X = np.arange(1, 100).reshape(-1,1)
+# weights = np.array([50,1,2,3,4,5]).reshape(-1, 1)
+#
+#
+# df = pd.DataFrame(X)
+#
+# my_list = list(df.rolling(5).mean())
+#
+# for item in my_list:
+#     if len(item) == 5:
+#         print(item.values * weights)
+
+
--- s8.py
+++ s8.py
@@ -20,7 +20,6 @@
 from datetime import datetime, timedelta
 import pandas as pd
 
-
 # dt = datetime.now()
 # print(dt)
 #
@@ -36,7 +35,7 @@ import pandas as pd
 # df = pd.DataFrame(test)
 #
 # df["Time"] = df["Time"].apply(minus_7)
-
+#
 #
 # test = [{"Time": 1},
 #          {"Time": 2},
@@ -48,31 +47,136 @@ import pandas as pd
 # print(df)
 #
 # df = pd.read_csv("data.csv")
+# print(df)
+# df.describe()
+# df.fillna(100, inplace=True)
+# df.fillna(method="ffill",inplace=True )
+# df.fillna(method="bfill", inplace=True )
+
+
+
+from sklearn.impute import SimpleImputer, KNNImputer
+import numpy as np
+
+df = pd.read_csv("data.csv")
+# impute = SimpleImputer(missing_values=11, strategy="mean")
+# impute = SimpleImputer(missing_values=np.nan, strategy="median")
+# impute = SimpleImputer(missing_values=np.nan, strategy="most_frequent")
+# impute = SimpleImputer(missing_values=np.nan, strategy="constant", fill_value=10)
 #
-# # df.fillna(method="ffill",inplace=True )
-# # df.fillna(method="bfill", inplace=True )
-# #
-# #
-# # print(df)
-# # print(df.describe())
-#
-# from sklearn.impute import SimpleImputer, KNNImputer
-# import numpy as np
-#
-# # imput = KNNImputer(n_neighbors=5)
-# # print(imput.fit_transform(df))
-# #
-# # print(df)
-#
-# df.where(df["a"]>5, 0, inplace=True)
-# impute = SimpleImputer(missing_values=np.nan, strategy="mean")
+# imput = KNNImputer(n_neighbors=5)
 # print(impute.fit_transform(df))
 
+# print(df)
+#
+# df.where(df["a"]>15, np.nan, inplace=True)
+# impute = SimpleImputer(missing_values=np.nan, strategy="mean")
+#
+# print(impute.fit_transform(df))
 
-import numpy as np
+# import numpy as np
+# X = [1,2,3,4,5]
+#
+# print(np.mean(X))
+# print(np.median(X))
+
+import os
+import cv2 as cv
 import matplotlib.pyplot as plt
+import numpy as np
 
-x = [1,1,2,2,3,2,2,1,1]*5
+# image_list = []
+#
+# for file in os.listdir("dataset"):
+#     img = cv.imread(f"dataset/{file}", cv.IMREAD_GRAYSCALE)
+#     image_list.append(img)
+#
+# image_list = np.array(image_list)
+# np.savez_compressed("image_dataset.npz", images = image_list)
 
-plt.plot(x)
+data = np.load("image_dataset.npz")
+images = data["images"]
+
+
+
+# print(images.shape)
+# plt.imshow(images[17], cmap="gray")
+# plt.show()
+#
+# for image in images:
+#     result = np.zeros_like(image)
+#     plt.subplot(1, 3, 2)
+#     vector = np.ravel(image)
+#     n = len(vector)
+#     mean = np.mean(vector)
+#     std = np.std(vector)
+#     up_coef = 3
+#     down_coef = 3
+#     up_line = mean+std*up_coef
+#     down_line = mean-std*down_coef
+#
+#     plt.plot(vector)
+#     plt.plot([0, n], [mean,mean], 'g--')
+#     plt.plot([0,n], [up_line, up_line], "r--")
+#     plt.plot([0,n], [down_line, down_line ], "r--")
+#
+#
+#     plt.subplot(1, 3, 1)
+#     plt.imshow(image, cmap="gray")
+#
+#     plt.subplot(1, 3, 3)
+#     result = np.ravel(result)
+#     result[vector>up_line] = 255
+#     result[vector<down_line] = 255
+#     result = result.reshape(image.shape)
+#     plt.imshow(result, cmap="gray")
+#
+#     plt.show()
+
+
+print(images.shape)
+plt.imshow(images[17], cmap="gray")
 plt.show()
+
+for image in images:
+    result = np.zeros_like(image)
+    plt.subplot(1, 3, 2)
+    vector = np.ravel(image)
+    n = len(vector)
+    model = np.poly1d(np.polyfit(np.arange(len(vector)), vector, 1))
+    reg = model(vector)
+
+    std = np.std(vector)
+    up_coef = 3
+    down_coef = 3
+    up_line = reg+std*up_coef
+    down_line = reg-std*down_coef
+
+    plt.plot(vector)
+    plt.plot([0, n], [reg,reg], 'g--')
+    plt.plot([0,n], [up_line, up_line], "r--")
+    plt.plot([0,n], [down_line, down_line ], "r--")
+
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(image, cmap="gray")
+
+    plt.subplot(1, 3, 3)
+    result = np.ravel(result)
+    result[vector>up_line] = 255
+    result[vector<down_line] = 255
+    result = result.reshape(image.shape)
+    plt.imshow(result, cmap="gray")
+
+    plt.show()
+
+
+
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# x = [1,1,2,2,3,2,2,1,1]*5
+#
+# plt.plot(x)
+# plt.show()

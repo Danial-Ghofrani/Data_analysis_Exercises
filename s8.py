420fb5130d5c935aec1485874bde43e68810db0e
Danial Ghofrani <danialghofrani2004@gmail.com>
2024-09-20 21:41:17 +0330

    s8

--- s8.py
+++ s8.py
@@ -134,49 +134,82 @@ images = data["images"]
 #     plt.show()
 
 
-print(images.shape)
-plt.imshow(images[17], cmap="gray")
-plt.show()
+# print(images.shape)
+# plt.imshow(images[17], cmap="gray")
+# plt.show()
 
 for image in images:
     result = np.zeros_like(image)
     plt.subplot(1, 3, 2)
     vector = np.ravel(image)
     n = len(vector)
-    model = np.poly1d(np.polyfit(np.arange(len(vector)), vector, 1))
-    reg = model(vector)
+    # model = np.poly1d(np.polyfit(np.arange(len(vector)), vector, 1))
+    # reg = model(vector)
 
-    std = np.std(vector)
-    up_coef = 3
-    down_coef = 3
-    up_line = reg+std*up_coef
-    down_line = reg-std*down_coef
+    # std = np.std(vector)
+    # up_coef = 3
+    # down_coef = 3
+    # up_line = reg+std*up_coef
+    # down_line = reg-std*down_coef
+    #
+    # plt.plot(vector)
+    # plt.plot([0, n], [reg,reg], 'g--')
+    # plt.plot([0,n], [up_line, up_line], "r--")
+    # plt.plot([0,n], [down_line, down_line ], "r--")
+    #
+    #
+    # plt.subplot(1, 3, 1)
+    # plt.imshow(image, cmap="gray")
+    #
+    # plt.subplot(1, 3, 3)
+    # result = np.ravel(result)
+    # result[vector>up_line] = 255
+    # result[vector<down_line] = 255
+    # result = result.reshape(image.shape)
+    # plt.imshow(result, cmap="gray")
+    #
+    # plt.show()
+    #
 
-    plt.plot(vector)
-    plt.plot([0, n], [reg,reg], 'g--')
-    plt.plot([0,n], [up_line, up_line], "r--")
-    plt.plot([0,n], [down_line, down_line ], "r--")
 
 
-    plt.subplot(1, 3, 1)
-    plt.imshow(image, cmap="gray")
+import numpy as np
+import matplotlib.pyplot as plt
 
-    plt.subplot(1, 3, 3)
-    result = np.ravel(result)
-    result[vector>up_line] = 255
-    result[vector<down_line] = 255
-    result = result.reshape(image.shape)
-    plt.imshow(result, cmap="gray")
-
-    plt.show()
-
-
-
-
-# import numpy as np
-# import matplotlib.pyplot as plt
-#
 # x = [1,1,2,2,3,2,2,1,1]*5
 #
 # plt.plot(x)
 # plt.show()
+
+
+# x = np.array([6, 1, 4])
+# print(np.tile(x, 10))
+# print(np.repeat(x, 10))
+# plt.plot(np.tile(x, 10))
+# plt.plot(np.repeat(x, 10))
+# plt.show()
+
+
+# scores = np.asarray_chkfinite([-3, 8, 12, -19, 16, 22, 17, 24, 9])
+# print(scores)
+
+# scores[scores<0] = 0
+# scores[scores>20] = 20
+
+# print(np.clip(scores, 0, 20))
+
+
+numbers = np.array([10, 12, 17])
+print(np.pad(numbers, [3,5], mode="constant", constant_values=[2,4]))
+print(np.pad(numbers, [3,5], mode="edge"))
+print(np.pad(numbers, [3,5], mode="reflect"))
+print(np.pad(numbers, [3,5], mode="linear_ramp"))
+
+plt.plot(np.pad(numbers, [3,5], mode="linear_ramp"))
+plt.show()
+
+
+
+
+
+
